6a7efdfbef76f4ab628541851f8e2cbd6f7824a4
MathicsWay <MathyWay@users.noreply.github.com>
2023-04-02 15:58:06 +0300

    Check first half algorithm work

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+int i = 0;
+int count = 0;
+while (i < array.Length)
+{
+    if(array[i].Length <= 3) count++;
+    i++;
+}
+Console.WriteLine($"Количество элементов равно {count}");
