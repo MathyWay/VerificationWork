a9e7368c65807eaee6993deb37196896b36715c4
MathicsWay <MathyWay@users.noreply.github.com>
2023-04-02 17:18:39 +0300

    Wrote README file

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿string[] array = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                                    .Where<string>(str => str.Length <= 3).ToArray();
+                                  .Where<string>(str => str.Length <= 3).ToArray();
 foreach(string? item in array)
 {
     Console.Write($"{item} ");
 }
-Console.WriteLine();
+Console.WriteLine(); 
